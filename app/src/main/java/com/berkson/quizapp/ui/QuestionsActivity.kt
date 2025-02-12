abd73c6e7ad51f5e273686ebfe4a61eff399b96e
Berkson Ximenes <berksonx@yahoo.com.br>
2025-02-12 20:16:00 -0300

    mostrando quantidade de questões no logcat

--- app/src/main/java/com/berkson/quizapp/ui/QuestionsActivity.kt
+++ app/src/main/java/com/berkson/quizapp/ui/QuestionsActivity.kt
@@ -1,11 +1,13 @@
 package com.berkson.quizapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.berkson.quizapp.R
+import com.berkson.quizapp.utils.Constants
 
 class QuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +19,8 @@ class QuestionsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val questions = Constants.getQuestions()
+        Log.d("QuestionSize", "${questions.size}")
     }
 }
