2702d2679bc09238fb5ee47783143b86ba8c3836
berkson <berksonx@yahoo.com.br>
2025-02-13 09:51:05 -0300

    criar função de reset de tela e função para muar a tela ao selecionar uma opção

--- app/src/main/java/com/berkson/quizapp/ui/QuestionsActivity.kt
+++ app/src/main/java/com/berkson/quizapp/ui/QuestionsActivity.kt
@@ -1,28 +1,36 @@
 package com.berkson.quizapp.ui
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.berkson.quizapp.R
 import com.berkson.quizapp.model.Question
 import com.berkson.quizapp.utils.Constants
+import com.google.android.material.button.MaterialButton
 
-class QuestionsActivity : AppCompatActivity() {
+class QuestionsActivity : AppCompatActivity(), OnClickListener {
     private lateinit var progressBar: ProgressBar
     private lateinit var textViewProgress: TextView
     private lateinit var textViewQuestion: TextView
     private lateinit var flagImage: ImageView
     private lateinit var textViewOptionOne: TextView
     private lateinit var textViewOptionTwo: TextView
-    private lateinit var textViewOptionthree: TextView
+    private lateinit var textViewOptionThree: TextView
     private lateinit var textViewOptionFour: TextView
+    private lateinit var checkButton: MaterialButton
     private val currentPosition = 1
     private lateinit var questionsList: MutableList<Question>
+    private var selectedOptionPosition = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,8 +48,15 @@ class QuestionsActivity : AppCompatActivity() {
         flagImage = findViewById(R.id.image_flag)
         textViewOptionOne = findViewById(R.id.text_view_option_one)
         textViewOptionTwo = findViewById(R.id.text_view_option_two)
-        textViewOptionthree = findViewById(R.id.text_view_option_three)
+        textViewOptionThree = findViewById(R.id.text_view_option_three)
         textViewOptionFour = findViewById(R.id.text_view_option_four)
+        checkButton = findViewById(R.id.button_check)
+
+        textViewOptionOne.setOnClickListener(this)
+        textViewOptionTwo.setOnClickListener(this)
+        textViewOptionThree.setOnClickListener(this)
+        textViewOptionFour.setOnClickListener(this)
+        checkButton.setOnClickListener(this)
 
 
         questionsList = Constants.getQuestions()
@@ -57,7 +72,64 @@ class QuestionsActivity : AppCompatActivity() {
         textViewQuestion.text = question.question
         textViewOptionOne.text = question.optionOne
         textViewOptionTwo.text = question.optionTwo
-        textViewOptionthree.text = question.optionThree
+        textViewOptionThree.text = question.optionThree
         textViewOptionFour.text = question.optionFour
+
+        if (currentPosition == questionsList.size) {
+            checkButton.text = getString(R.string.concluir).uppercase()
+        } else {
+            checkButton.text = getString(R.string.check).uppercase()
+        }
+    }
+
+    private fun resetOptions() {
+        val options = mutableListOf<TextView>()
+        options.add(textViewOptionOne)
+        options.add(textViewOptionTwo)
+        options.add(textViewOptionThree)
+        options.add(textViewOptionFour)
+
+        for (o in options) {
+            o.setTextColor(Color.parseColor("#7A8089"))
+            o.typeface = Typeface.DEFAULT
+            o.background = ContextCompat.getDrawable(
+                this, R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    private fun selectedOption(textView: TextView, selectedOptionNumber: Int) {
+        resetOptions()
+        selectedOptionPosition = selectedOptionNumber
+
+        textView.setTextColor(Color.parseColor("#363A43"))
+        textView.setTypeface(textView.typeface, Typeface.BOLD)
+        textView.background = ContextCompat.getDrawable(
+            this, R.drawable.selected_option_border_bg
+        )
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.text_view_option_one -> {
+                selectedOption(textViewOptionOne, 1)
+            }
+
+            R.id.text_view_option_two -> {
+                selectedOption(textViewOptionTwo, 2)
+            }
+
+            R.id.text_view_option_three -> {
+                selectedOption(textViewOptionThree, 3)
+            }
+
+            R.id.text_view_option_four -> {
+                selectedOption(textViewOptionFour, 4)
+            }
+
+            R.id.button_check -> {
+                //TODO: checar de está correto
+            }
+        }
     }
 }
