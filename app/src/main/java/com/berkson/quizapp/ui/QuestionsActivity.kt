69220f765a618a65fa88805f0f2ce2ba104205fb
Berkson Ximenes <berksonx@yahoo.com.br>
2025-02-12 21:31:28 -0300

    concluir estrutura da view de perguntas e iniciar ligação com o back do componente

--- app/src/main/java/com/berkson/quizapp/ui/QuestionsActivity.kt
+++ app/src/main/java/com/berkson/quizapp/ui/QuestionsActivity.kt
@@ -2,6 +2,9 @@ package com.berkson.quizapp.ui
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,15 @@ import com.berkson.quizapp.R
 import com.berkson.quizapp.utils.Constants
 
 class QuestionsActivity : AppCompatActivity() {
+    private lateinit var progressBar: ProgressBar
+    private lateinit var textViewProgress: TextView
+    private lateinit var textViewQuestion: TextView
+    private lateinit var flagImage: ImageView
+    private lateinit var textViewOptionOne: TextView
+    private lateinit var textViewOptionTwo: TextView
+    private lateinit var textViewOptionthree: TextView
+    private lateinit var textViewOptionFour: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
