3f6f201778dc89ee72c1367f1db6be161371205b
berkson <berksonx@yahoo.com.br>
2025-02-15 20:03:41 -0300

    corrigir adjustResize da tela inicial que não funcionava corretamente

--- app/src/main/java/com/berkson/quizapp/MainActivity.kt
+++ app/src/main/java/com/berkson/quizapp/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
